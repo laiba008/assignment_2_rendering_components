62866dd22dfc2a789a7da777bc28d5e36f104432
laiba008 <laibanaseer008@gmail.com>
2024-11-22 22:23:56 +0500

    first commit

---
+++ src/app/components/about.tsx
@@ -0,0 +1,8 @@
+export default function About () {
+    return (
+        <>
+        <h3>About</h3>
+        <p>Hi, I'm Laiba, an Intermediate Pre-Medical student with a keen interest in Generative AI. I'm currently expanding my skills through the Governor House program.</p>
+        </>
+    )
+}
---
+++ src/app/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <>
+        <h2>Header</h2>
+        <p>Class 2 Assignment of Next.js</p>
+        </>
+    )
+}
